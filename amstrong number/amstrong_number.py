26ef88fd1819bf2d5e497470ff8ae2a942acc4e6
Kiruthigakiruthiga18 <144360132+Kiruthigakiruthiga18@users.noreply.github.com>
2025-10-26 00:47:57 -0700

    Improve readability of Armstrong number code
    Refactor Armstrong number logic for clarity.

--- amstrong number/amstrong_number.py
+++ amstrong number/amstrong_number.py
@@ -1,10 +1,10 @@
-num = int(input("Enter the number: "))
+num = int(input("Enter the number: "))#enter the number
 num1=num
 sum1= 0
-power = len(str(num))
-while num!=0:
-                    val=num % 10
-                    sum1+=(val**power)
+power = len(str(num))#count the lenght of the number
+while num!=0:# it check the condition untill the number is 0
+                    val=num % 10  # remainder in val
+                    sum1+=(val**power) 
                     num //= 10
 if num1==sum1:
                     print(" armstrong number ",num1)
