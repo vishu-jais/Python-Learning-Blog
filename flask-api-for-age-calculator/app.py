17fd87acfb25b8bf1d99d33d3e34216fd66a9288
Praveenkanth G <praveenkanth2805@gmail.com>
2025-10-26 18:56:20 +0530

    add comments in app.py

--- flask-api-for-age-calculator/app.py
+++ flask-api-for-age-calculator/app.py
@@ -1,13 +1,16 @@
+#import section
 from flask import Flask, render_template, request, flash, redirect,url_for
 from datetime import date
 
 app = Flask(__name__)
-app.secret_key='my_super_key1728'
+app.secret_key='my_super_key1728'    #super key is make session is secure
 
+#default home page route
 @app.route('/')
 def home():
     return render_template('index.html')
 
+#get user dob from frent-end
 @app.route('/submit', methods=['POST'])
 def submit():
     dob = request.form.get('dob')
@@ -19,17 +22,20 @@ def submit():
     date=dob[-2:]
     return redirect(url_for('calc', by=year,bm=month,bd=date))
 
+#calculation route
 @app.route('/calc')
 def calc():
     by=request.args.get('by')
     bm=request.args.get('bm')  
     bd=request.args.get('bd')
-    
+
+    #get system date 
     today = date.today()
     td=today.day
     tm=today.month
     ty=today.year
-    
+
+    #calculate the age
     if ty<int(by):
         msg='please give valied year'
         return render_template('index.html',msg=msg)
@@ -48,5 +54,5 @@ def calc():
    
     result=f"you age {yob} years, {mob} months, {dob} days."
     return render_template('index.html',result=result)
-
+#app run
 app.run(debug=True)
