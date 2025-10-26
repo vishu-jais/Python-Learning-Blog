c7b9cf3b31ef2b06f5b6a86ee39db583b47e13d6
anisha-a29 <rubishmagimagi@gmail.com>
2025-10-26 12:39:32 +0530

    Remove comments and clean up greet function calls

--- default_function-1.py
+++ default_function-1.py
@@ -1,10 +1,8 @@
-# A Python program to demonstrate default and keyword arguments in functions.
+
 
 def greet(name="User", message="Welcome!"):
     print(f"Hello {name}, {message}")
-
-# Calling the function in different ways
-greet()                          # Uses both default values
-greet("Anu")                     # Overrides only name
-greet(message="Good Morning!")   # Overrides only message
-greet("Riya", "Have a nice day!") # Overrides both
+greet()                         
+greet("Anu")                   
+greet(message="Good Morning!")   
+greet("Riya", "Have a nice day!") 
