f01ba5b49494aa6c7f488efcd371079de2890301
deepikasekar232005-blip <deepikasekar232005@gmail.com>
2025-10-28 20:29:42 +0530

    Update Arithmetic.py

--- Arithmetic/Arithmetic.py
+++ Arithmetic/Arithmetic.py
@@ -1,7 +1,22 @@
+# program to perform basic arithmetic operation between two numbers
+
+# Taking first number as input from user
 a = int(input("Enter the number1:"))
+
+# Taking second number as input from user
 b = int(input("Enter the number2:"))
+
+# Performing and displaying addition
 print("Addition:",a+b)
+
+# Performing and displaying substration
 print("Subtraction:",a-b)
+
+# performing and displaying muliplication
 print("Multiplication:",a*b)
+
+# Performing and displaying division
 print("Division:",a/b)
+
+# performing and displaying modulus(remainder)
 print("Modulus:",a%b)
