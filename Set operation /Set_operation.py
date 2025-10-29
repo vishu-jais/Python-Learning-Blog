a8061ed2c0096e0cc1e80ed13035100056c8ce3f
Jayavarthini karthikeyan <vijiviji0842@gmail.com>
2025-10-29 18:29:13 +0530

    Implement set operations for union and intersection

---
+++ Set operation /Set_operation.py
@@ -0,0 +1,11 @@
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+union_set = A | B
+intersection_set = A & B
+
+print("Set A:", A)
+print("Set B:", B)
+print("Union:", union_set)
+print("Intersection:", intersection_set)
