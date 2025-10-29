a54351d50b68c3db99d9ea0aa9ced84fd3a9210c
Deepakraghavan01 <57013229+Deepakraghavan01@users.noreply.github.com>
2025-10-29 22:12:42 +0530

    Delete Recursive string reversal.py

--- Recursive string reversal.py
+++
@@ -1,8 +0,0 @@
-def reverse_string(s):
-    if len(s) == 0:
-        return s
-    else:
-        return reverse_string(s[1:]) + s[0]
-text = input("Enter a string: ")
-result = reverse_string(text)
-print("Reversed string:", result)
